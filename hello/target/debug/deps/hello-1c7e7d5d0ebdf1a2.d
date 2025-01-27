168caf5369a9cd8d52b234fc964a296dbd8d1a2b
Daniel Zhang <dzahng@unc.edu>
2025-01-27 04:46:05 +0000

    Successfully ran project

---
+++ hello/target/debug/deps/hello-1c7e7d5d0ebdf1a2.d
@@ -0,0 +1,5 @@
+/workspaces/RustExample/hello/target/debug/deps/libhello-1c7e7d5d0ebdf1a2.rmeta: src/main.rs
+
+/workspaces/RustExample/hello/target/debug/deps/hello-1c7e7d5d0ebdf1a2.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/deps/hello-974ecc9fe32f333e.d
@@ -0,0 +1,5 @@
+/workspaces/RustExample/hello/target/debug/deps/libhello-974ecc9fe32f333e.rmeta: src/main.rs
+
+/workspaces/RustExample/hello/target/debug/deps/hello-974ecc9fe32f333e.d: src/main.rs
+
+src/main.rs:
